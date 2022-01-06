d3a5993c648ae639b7b6131cc66554291b5e9d77
felipeguerreiro7 <lf.guerreiroalves@gmail.com>
2022-01-06 10:13:27 -0300

    Entregando mais 4 exercicios

---
+++ exercicio 10/index.js
@@ -0,0 +1,6 @@
+let velocidaDoCarro = 80
+if(velocidaDoCarro > 80){
+    console.log("levou multa")
+} else {
+    console.log("Dentro do permitido")
+}
---
+++ exercicio 11/index.js
@@ -0,0 +1,8 @@
+let idade = 18
+let cnh = false
+
+if(idade >= 18 && cnh === true){
+    console.log("pode dirigir")
+} else {
+    console.log("n pode dirigir")
+}

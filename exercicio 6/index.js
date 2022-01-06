0c8154a2251e89510314b85e8249d1136f59e56d
felipeguerreiro7 <lf.guerreiroalves@gmail.com>
2022-01-06 09:48:00 -0300

    3 exercicios feitos

---
+++ exercicio 6/index.js
@@ -0,0 +1,7 @@
+let numberTest = 5
+let stringTest = "Qualquer coisa"
+let booleanTest = ((5 > 4))
+
+console.log(numberTest)
+console.log(stringTest)
+console.log(booleanTest)
---
+++ exercicio 7/index.js
@@ -0,0 +1,4 @@
+let idade = prompt("Voce tem quantos anos?")
+if(idade >= 18){
+    console.log("Pode entrar")
+}
